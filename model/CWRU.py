5820127cd5b0e1fb3ca86cb1953d60411e5d993d
jingwang <j329333767@gmail.com>
2023-06-27 15:34:05 -0700

    spatial

--- model/CWRU.py
+++ model/CWRU.py
@@ -65,13 +65,13 @@ class ChannelPool(nn.Module):
 class SpatialGate(nn.Module):
     def __init__(self):
         super(SpatialGate, self).__init__()
-        kernel_size = 5
+        kernel_size = 3
         self.compress = ChannelPool()
         self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
     def forward(self, x):
         x_compress = self.compress(x)
         x_out = self.spatial(x_compress)
-        scale = F.sigmoid(x_out) # broadcasting
+        scale = F.sigmoid(10*x_out) # broadcasting
         return x * scale
 
 
