84fe8f4737d2cf615e465f5e2cdb9399270bdc45
jingwang <j329333767@gmail.com>
2023-06-27 15:05:30 -0700

    spatial

--- model/CWRU.py
+++ model/CWRU.py
@@ -39,7 +39,39 @@ class ChannelGate(nn.Module):
             else:
                 channel_att_sum = channel_att_sum + channel_att_raw
 
-        scale = F.sigmoid(100*channel_att_sum).unsqueeze(2).expand_as(x) # channel_att_sum.shape -> [64, 64]
+        scale = F.sigmoid(10*channel_att_sum).unsqueeze(2).expand_as(x) # channel_att_sum.shape -> [64, 64]
+        return x * scale
+
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv1d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat((torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1)
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = F.sigmoid(x_out) # broadcasting
         return x * scale
 
 
@@ -54,12 +86,15 @@ class Feature(nn.Module):
         self.maxpool = nn.AvgPool1d(stride=2, kernel_size=2)
 
         self.channel_1 = ChannelGate(32, pool_types=['avg', 'max'])
+        self.SpatialGate = SpatialGate()
+
         # self.channel_1 = ChannelGate(32, pool_types=['max'])
         # self.channel_2 = ChannelGate(64, pool_types=['avg', 'max'])
 
     def forward(self, x, is_deconv=False):
         x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
         x = self.channel_1(x)
+        x = self.SpatialGate(x)
         x = self.maxpool(self.relu(self.bn21(self.conv21(x))))
         # x = self.channel_2(x)
         return x
