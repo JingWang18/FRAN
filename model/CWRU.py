db381b999c7df82c29cdde588888db7a5aa10ce3
jingwang <j329333767@gmail.com>
2023-06-30 14:23:30 -0700

    spatial

--- model/CWRU.py
+++ model/CWRU.py
@@ -98,8 +98,8 @@ class Feature(nn.Module):
         # self.channel_2 = ChannelGate(64, pool_types=['avg', 'max'])
 
     def forward(self, x, is_target=False):
-        x= F.normalize(x)
-        x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        # x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        x = self.maxpool(self.conv1(x))
         # x = self.channel_1(x)
         x = self.SpatialGate(x, is_target)
         x = self.maxpool(self.relu(self.bn21(self.conv21(x))))
