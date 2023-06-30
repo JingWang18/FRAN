41aa8ce35814779fa6bc0c2845ba762a08a90fdc
jingwang <j329333767@gmail.com>
2023-06-30 15:03:29 -0700

    spatial no channel compress

--- model/CWRU.py
+++ model/CWRU.py
@@ -64,7 +64,7 @@ class BasicConv(nn.Module):
 
 class ChannelPool(nn.Module):
     def forward(self, x):
-        return torch.cat((torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1), torch.std(x,1)), dim=1)
+        return torch.cat((torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1), torch.std(x,1).unsqueeze(1)), dim=1)
 
 class SpatialGate(nn.Module):
     def __init__(self):
