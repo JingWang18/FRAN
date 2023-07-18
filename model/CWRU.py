6cfc9fb231233f1bbe00f1f6d0a76083e722d805
jingwang <j329333767@gmail.com>
2023-07-17 19:02:56 -0700

    change main.py

--- model/CWRU.py
+++ model/CWRU.py
@@ -114,12 +114,12 @@ class Feature(nn.Module):
         # x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
         # Wavelet transform with 3 levels
         x,_ = self.dwt_1(x)
-        x = self.maxpool(self.bn1(self.conv1(x)))
+        x = self.maxpool(self.conv1(x))
         # x = self.channel_1(x)
         x,_ = self.dwt_2(x)
-        x = self.maxpool(self.bn21(self.conv21(x)))
+        x = self.maxpool(self.conv21(x))
 
-        x = self.SpatialGate(x)
+        # x = self.SpatialGate(x)
 
         # x = self.SpatialGate(x, is_target)
         # x = self.SpatialGate(x)
