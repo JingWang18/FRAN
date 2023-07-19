33a11652fbe61889d0b1ed91ac76e502b963fa19
jingwang <j329333767@gmail.com>
2023-07-19 11:14:29 -0700

    DTCWT.py

--- model/CWRU.py
+++ model/CWRU.py
@@ -106,7 +106,6 @@ class Feature(nn.Module):
         # self.dwt_2 = DWT1DForward(wave='db6', J=1).cuda()
 
         self.dwt_1 = DTCWTForward(J=1, biort='near_sym_b', qshift='qshift_b').cuda()
-        self.dwt_2 = DTCWTForward(J=1, biort='near_sym_b', qshift='qshift_b').cuda()
 
         # self.channel_1 = ChannelGate(32, pool_types=['max'])
         # self.channel_2 = ChannelGate(64, pool_types=['avg', 'max'])
@@ -122,11 +121,11 @@ class Feature(nn.Module):
         x = self.maxpool(self.conv11(x)+x_0)
         x = self.bn1(self.conv12(x))
 
-        x_1 = x
+        # x_1 = x
         # x = self.channel_1(x)
-        x,_ = self.dwt_2(x)
-        x = self.maxpool(self.conv21(x)+x_1)
-        x = self.bn2(self.conv22(x))
+        # x,_ = self.dwt_2(x)
+        # x = self.maxpool(self.conv21(x)+x_1)
+        x = self.maxpool(self.bn2(self.conv22(x)))
 
         # x = self.SpatialGate(x)
 
